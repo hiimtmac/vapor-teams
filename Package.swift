5af56e97ca26a269a57ac5d3c1791c5192601de9
hiimtmac <hiimtmac+github@gmail.com>
2023-09-11 14:53:54 -0500

    update

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "VaporTeams", targets: ["VaporTeams"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.77.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.81.0"),
         .package(url: "https://github.com/hiimtmac/chat-kit.git", branch: "main"),
     ],
     targets: [
